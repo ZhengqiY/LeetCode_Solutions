76929f30346c75270fc52dbef91e47feb17e9b3d
ZhengqiY <51040140+ZhengqiY@users.noreply.github.com>
2020-03-22 22:57:35 -0700

    QuickSortApproach

--- Heap/KClosest.cpp
+++ Heap/KClosest.cpp
@@ -25,4 +25,45 @@ public:
 		}
 		return res;
 	}
+//-----------------------------------------------Quick Sort----------------------------
+public:
+	vector<vector<int>> kClosestQuickSort(int k, vector<vector<int>>& points)
+	{
+		int low = 0;
+		int high = points.size()-1;
+		quickSort(points, low, high);
+		vector<vector<int>> res = vector<vector<int>> (points.begin(), points.begin() + k);
+		return res;
+	}
+
+	void quickSort(vector<vector<int>>& points, int low, int high)
+	{
+		if (low < high) {
+			int s = partition(points, low, high);
+			quickSort(points,low, s - 1);
+			quickSort(points, s + 1, high);
+		}
+	}
+	int partition(vector<vector<int>>& points, int low, int high)
+	{
+		int left = low, right = high;
+		int pivot = dist(points[low]);
+		while (true) {
+			while (left < high && dist(points[left]) <= pivot) {
+				left++;
+			}
+			while (right > low && dist(points[right]) >= pivot) {
+				right--;
+			}
+			if (left >= right) break;
+			swap(points[left], points[right]);
+		}
+		swap(points[low], points[right]);
+		return right;
+	}
+	int dist(vector<int> &pairs)
+	{
+		return pow(pairs[0], 2) + pow(pairs[1], 2);
+	}
+
 };
